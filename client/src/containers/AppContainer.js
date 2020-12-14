eddbd056489254917ae0bc110e32a1a3c0c0d8d0
svvishjs <andreww92@live.com>
2020-12-14 10:21:27 +0000

    added FindBooksPage upper component

--- client/src/containers/AppContainer.js
+++ client/src/containers/AppContainer.js
@@ -1,11 +1,13 @@
-import NewUserBox from './NewUserBox';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NewUserBox from './NewUserBox';
+import FindBooksPage from './FindBooksPage';
 
 const AppContainer = () => {
 	return (
 		<Router>
 			<Switch>
 				<Route exact path="/sign-up" component={NewUserBox} />
+				<Route path="/find-books" component={FindBooksPage} />
 			</Switch>
 		</Router>
 	);
---
+++ client/src/containers/FindBooksPage.js
@@ -0,0 +1,9 @@
+const FindBooksPage = () => {
+	return (
+		<>
+			<h1>FIND BOOKS</h1>
+		</>
+	);
+};
+
+export default FindBooksPage;
