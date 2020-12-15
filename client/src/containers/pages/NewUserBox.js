8a33ad8cc30b39cbe3eb8d9fb8f444e0be8f29f0
svvishjs <andreww92@live.com>
2020-12-15 22:02:28 +0000

    added new functionality which asks a user if theyve signed up before and then takes them to the correct page

--- client/src/containers/pages/NewUserBox.js
+++ client/src/containers/pages/NewUserBox.js
@@ -6,38 +6,81 @@ import { getUsers, postUser } from '../../fetches/UserFetch';
 
 const NewUserBox = () => {
 	const [users, setUsers] = useState([]);
+	const [userChange, setUserChange] = useState(0);
+	const [showSignUp, setShowSignUp] = useState(false);
+	const [showSelect, setShowSelect] = useState(false);
 
 	const addNewUser = (newUser) => {
 		// POSTS IT TO BACKEND API & REDIRECTS TO SEARCH PAGE OR MY BOOKS PAGE
 		postUser(newUser);
+		setUserChange(userChange + 1);
 	};
+
+	const showAllUsers = users.map((user, index) => {
+		return (
+			<div key={index}>
+				<h6>
+					Name: {user.firstName}&nbsp;{user.lastName}
+				</h6>
+				<h6>Community: {user.community}</h6>
+				<h6>Email: {user.email}</h6> |
+			</div>
+		);
+	});
 
 	useEffect(() => {
 		getUsers().then((data) => setUsers(data));
-	}, []);
+	}, [userChange]);
+
+	const handleYesClick = () => {
+		setShowSelect(true);
+		setShowSignUp(false);
+	};
+
+	const handleNoClick = () => {
+		setShowSignUp(true);
+		setShowSelect(false);
+	};
+
+	const renderItems = () => {
+		if (showSelect === false && showSignUp === false) {
+			return (
+				<>
+					<div>
+						<h2>Signed Up Before?</h2>
+					</div>
+					<div>
+						<button onClick={handleYesClick}>YES</button>
+						<button onClick={handleNoClick}>NO</button>
+					</div>
+				</>
+			);
+		} else if (showSelect === true) {
+			return (
+				<>
+					<div>
+						<h2>Click your name below:</h2>
+					</div>
+					<div>{showAllUsers}</div>
+				</>
+			);
+		} else {
+			return (
+				<div>
+					<NewUserForm
+						onNewUserSubmit={(newUser) => {
+							addNewUser(newUser);
+						}}
+					/>
+				</div>
+			);
+		}
+	};
 
 	return (
 		<>
 			<Header />
-			<div>
-				<h2>Sign Up</h2>
-			</div>
-			<NewUserForm
-				onNewUserSubmit={(newUser) => {
-					addNewUser(newUser);
-				}}
-			/>
-			<div>
-				{users.map((user, index) => {
-					return (
-						<div key={index}>
-							<h6>{user.username}</h6>
-							<h6>{user.userEmail}</h6>
-							<h6>{user.userCommunity}</h6>
-						</div>
-					);
-				})}
-			</div>
+			<div>{renderItems()}</div>
 			<Footer />
 		</>
 	);
