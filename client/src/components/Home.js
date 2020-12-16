c3b3a1cf5b7cdb424ba1c3fd30a5c77f78106fd5
svvishjs <andreww92@live.com>
2020-12-16 21:48:23 +0000

    added buttons and sorted errors in homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,35 +5,33 @@ import NavBar from './NavBar';
 const Home = () => {
 	return (
 		<>
-			<div className="home-container">
-				<div className="home-upper">
-					<div className="home-main-text">
-						<p>The libraries are dead.</p>
-						<p>Long live the libraries.</p>
-					</div>
-					<div className="home-upper-btn">
-						<button className="btn-how-it-works">
-							<Link to="/how-it-works">How it Works</Link>
-						</button>
-					</div>
-					<div>
-						<img
-							className="img-down-arrow"
-							src={'../images/down-arrow.svg'}
-							alt={'down arrow'}
-						/>
-					</div>
+			<div className="home-upper">
+				<div className="home-main-text">
+					<p>The libraries are dead.</p>
+					<p>Long live the libraries.</p>
 				</div>
-				<div className="home-lower">
-					<div>
-						<button>
-							<Link to="/share-books">Share a Book</Link>
-						</button>
-					</div>
-					<button>
-						<Link to="/find-books">Find a Book</Link>
+				<div>
+					<button className="btn-how-it-works">
+						<Link to="/how-it-works">How it Works</Link>
 					</button>
 				</div>
+				<div>
+					<img
+						className="img-down-arrow"
+						src={'../images/down-arrow.svg'}
+						alt={'down arrow'}
+					/>
+				</div>
+			</div>
+
+			<div className="home-btns">
+				<button className="share-btn">
+					<Link to="/share-books">Share books</Link>
+				</button>
+
+				<button className="find-btn">
+					<Link to="/find-books">Find a Book</Link>
+				</button>
 			</div>
 		</>
 	);
