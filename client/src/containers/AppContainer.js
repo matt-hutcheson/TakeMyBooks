9cda57b48abe12f29e28b4996171a45710c4671f
Mathew-Tomaney <mat.tomaney@gmail.com>
2020-12-16 00:05:22 +0000

    add most of MyRequests basic page. need to sort currentUser at App level to test.

--- client/src/containers/AppContainer.js
+++ client/src/containers/AppContainer.js
@@ -2,12 +2,13 @@ import BookContainer from './BookContainer'
 import MyBooksContainer from './MyBookContainer'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import {useState, useEffect} from 'react';
-import {getUsers} from '../fetches/UserFetch';
+import {getUsers, getUsersById} from '../fetches/UserFetch';
 import Home from '../components/Home';
 import NavBar from '../components/NavBar';
 import NewUserBox from './pages/NewUserBox';
 import FindBooksPage from './pages/FindBooksPage';
 import AddBook from '../components/addBookPage/AddBook';
+import MyRequestsPage from '../containers/pages/MyRequestsPage';
 import './AppContainer.css'
 
 const AppContainer = () => {
@@ -15,10 +16,10 @@ const AppContainer = () => {
 	const [currentUser, setCurrentUser] = useState({});
 	const [users, setUsers] = useState([]);
 
-	// useEffect(() => {
-	// 	getUsers()
-	// 	.then(data => setUsers(data))
-	// }, [])
+	useEffect(() => {
+		getUsers()
+		.then(data => setUsers(data))
+	}, [])
 
 	return (
 		<Router>
@@ -32,6 +33,7 @@ const AppContainer = () => {
 					<Route path="/my-books" component={MyBooksContainer}/>
 					<Route path="/find-books" component={FindBooksPage} />
 					<Route path="/my-books/add-book" component={AddBook} />
+					<Route path="/my-requests" exact render={()=><MyRequestsPage currentUser={currentUser}/>}/>
 					{/* <Route component={ErrorPage}/> */}
 				</Switch>
 			</main>
--- client/src/containers/pages/MyRequestsPage.js
+++ client/src/containers/pages/MyRequestsPage.js
@@ -1,28 +1,35 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import RequestList from '../../components/myRequestsPage/RequestList';
 import { getRequestsByOwnerId, getRequestsByRequesterId } from '../../fetches/RequestFetch';
 
 const MyRequestsPage = ({currentUser}) => {
-    const [requestShare, setRequestShare] = useState([])
-    const [requestBorrow, setRequestBorrow] = useState([])
+    const [requestsShare, setRequestsShare] = useState([])
+    const [requestsBorrow, setRequestsBorrow] = useState([])
+    
 
     useEffect(() => {
-		getRequestsByOwnerId({currentUser}.id).then((data) => {
+		getRequestsByOwnerId({currentUser}).then((data) => {
 			setRequestsShare(data);
         });
-        getRequestsByRequesterId({currentUser}.id).then((data) => {
-            setRequestBorrow(data);
-        });
-    }, []);
-    
+    }, [currentUser]);
+    useEffect(()=>{
+    getRequestsByRequesterId({currentUser}).then((data) => {
+        setRequestsBorrow(data);
+    });
+    }, [currentUser]);
+
     return(
         <>
             <Header/>
             <h2>Books I'm Sharing:</h2>
-            <RequestList request={requestShare}/>
+            <RequestList currentUser={currentUser} requests={requestsShare}/>
             <h2>Books I'm Receiving:</h2>
-            <RequestList request={requestBorrow}/>
+            <RequestList currentUser={currentUser} requests={requestsBorrow}/>
             <Footer/>
         </>
     );
 };
+
+export default MyRequestsPage;
