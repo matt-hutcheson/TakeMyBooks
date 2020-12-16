4b6138b109ffd3d87e8cda8b71ce2842507fc235
svvishjs <andreww92@live.com>
2020-12-16 10:32:37 +0000

    minor change AppContainer

--- client/src/containers/AppContainer.js
+++ client/src/containers/AppContainer.js
@@ -1,17 +1,16 @@
-import BookContainer from './BookContainer'
-import MyBooksContainer from './MyBookContainer'
+import BookContainer from './BookContainer';
+import MyBooksContainer from './MyBookContainer';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import {useState, useEffect} from 'react';
-import {getUsers} from '../fetches/UserFetch';
+import { useState, useEffect } from 'react';
+import { getUsers } from '../fetches/UserFetch';
 import Home from '../components/Home';
 import NavBar from '../components/NavBar';
 import NewUserBox from './pages/NewUserBox';
 import FindBooksPage from './pages/FindBooksPage';
 import AddBook from '../components/addBookPage/AddBook';
-import './AppContainer.css'
+import './AppContainer.css';
 
 const AppContainer = () => {
-
 	const [currentUser, setCurrentUser] = useState({});
 	const [users, setUsers] = useState([]);
 
@@ -26,12 +25,24 @@ const AppContainer = () => {
 				<NavBar />
 				<Switch>
 					<Route path="/sign-up" component={NewUserBox} />
-					<Route exact path="/" component={Home}/>
+					<Route exact path="/" component={Home} />
 					<Route exact path="/books" component={BookContainer} />
-					<Route exact path="/book-detail" component={BookContainer} />
-					<Route exact path="/my-books" component={MyBooksContainer}/>
+					<Route
+						exact
+						path="/book-detail"
+						component={BookContainer}
+					/>
+					<Route
+						exact
+						path="/my-books"
+						component={MyBooksContainer}
+					/>
 					<Route exact path="/find-books" component={FindBooksPage} />
-					<Route path="/users/:id/books/add-book" exact render={() => <AddBook currentUser="currentUser" />} />
+					<Route
+						path="/users/:id/books/add-book"
+						exact
+						render={() => <AddBook currentUser="currentUser" />}
+					/>
 					{/* <Route component={ErrorPage}/> */}
 				</Switch>
 			</main>
