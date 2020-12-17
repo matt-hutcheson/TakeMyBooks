4fad33385233c62d738c9c59bb61cbfb7261a33e
svvishjs <andreww92@live.com>
2020-12-17 16:41:26 +0000

    removed some not needed code

--- client/src/components/findBooksPage/Book.js
+++ client/src/components/findBooksPage/Book.js
@@ -2,33 +2,12 @@ import React, { useState } from 'react';
 import '../../styles/BookResult.css';
 
 const Book = ({ book }) => {
-	// const [bookStatus, setBookStatus] = useState(false);
-
-	// const handleBookHover = (event) => {
-	// 	event.preventDefault();
-	// 	setBookStatus(true);
-	// };
-
-	// const handleBookLeave = (event) => {
-	// 	event.preventDefault();
-	// 	setBookStatus(false);
-	// };
-
 	return (
 		<div className="book-result">
-			<img
-				src={book.image}
-				alt="book cover"
-				// onMouseEnter={handleBookHover}
-				// onMouseLeave={handleBookLeave}
-			/>
+			<img src={book.image} alt="book cover" />
 			<div className="overlay">
-				{/* {bookStatus ? (
-					<> */}
 				<p className="text1">{book.title}</p>
 				<p className="text2">by {book.author}.</p>
-				{/* </>
-				) : null} */}
 			</div>
 
 			{/* <p className="book-genre">Genre: {book.genre}</p> */}
