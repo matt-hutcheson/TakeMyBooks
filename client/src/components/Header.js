ea4619d70a472cb83639635d8c5c83857f03c4f5
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-12-17 12:53:28 +0000

    fixed userdrop-down and login button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,40 @@
 import NavBar from './NavBar';
 import '../styles/Header.css';
 
-const Header = () => {
-	return (
+const Header = ({currentUser}) => {
+
+	if (Object.keys(currentUser).length === 0 && currentUser.constructor === Object){
+		return (
+			<>
+				<header>
+					<a href="/">
+						<img
+							className="header-logo"
+							src={'../images/BookYeetLogo.png'}
+							alt={'logo'}
+						></img>
+					</a>
+					<ul className="header-list">
+						<li>
+							<a href="/login">Login</a>
+						</li>
+						<li>
+							<a href="/about">About</a>
+						</li>
+						<li className="img-li">
+							<img
+								src={'../images/burger-menu.svg'}
+								alt={'nav bar menu'}
+								className="burger-image"
+							/>
+						</li>
+					</ul>
+				</header>
+				<NavBar />
+			</>
+		);
+	} else {
+		return (
 		<>
 			<header>
 				<a href="/">
@@ -14,7 +46,7 @@ const Header = () => {
 				</a>
 				<ul className="header-list">
 					<li>
-						<a href="/login">Login</a>
+						<a href="/login">Change User: {currentUser.userName}</a>
 					</li>
 					<li>
 						<a href="/about">About</a>
@@ -31,6 +63,7 @@ const Header = () => {
 			<NavBar />
 		</>
 	);
+	}
 };
 
 export default Header;
--- client/src/containers/pages/NewUserBox.js
+++ client/src/containers/pages/NewUserBox.js
@@ -3,13 +3,15 @@ import NewUserForm from '../../components/signUpPage/NewUserForm';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import { getUsers, postUser } from '../../fetches/UserFetch';
+import { Link } from 'react-router-dom';
 import '../../styles/NewUserBox.css'
 
-const NewUserBox = ({handleSelectUser}) => {
+const NewUserBox = ({setCurrentUser}) => {
 	const [users, setUsers] = useState([]);
 	const [userChange, setUserChange] = useState(0);
 	const [showSignUp, setShowSignUp] = useState(false);
 	const [showSelect, setShowSelect] = useState(false);
+	const [selectedUser, setSelectedUser] = useState({})
 
 	const addNewUser = (newUser) => {
 		// POSTS IT TO BACKEND API & REDIRECTS TO SEARCH PAGE OR MY BOOKS PAGE
@@ -19,7 +21,8 @@ const NewUserBox = ({handleSelectUser}) => {
 
 	const showAllUsers = () => {
 		return (
-			<select onChange={handleSelectUser}>
+			<select defaultValue="0" onChange={handleSelectUser}>
+				<option value="0" disabled="disabled">Please select your username</option>
 				{users.map((user, index) => {
 					return <option key={index} value={JSON.stringify(user)} >{user.userName}</option>
 				})}
@@ -42,6 +45,15 @@ const NewUserBox = ({handleSelectUser}) => {
 		setShowSelect(false);
 	};
 
+	const handleSelectUser = (event) => {
+		event.preventDefault();
+		setSelectedUser(JSON.parse(event.target.value));
+	};
+
+	const handleSelectClick = () => {
+		setCurrentUser(selectedUser);
+	};
+
 	const renderItems = () => {
 		if (showSelect === false && showSignUp === false) {
 			return (
@@ -62,6 +74,9 @@ const NewUserBox = ({handleSelectUser}) => {
 						<h2 className="question">Select your name below:</h2>
 					</div>
 					<div className="show-users">{showAllUsers()}</div>
+					<button className="" onClick={handleSelectClick}>
+						<Link to="/">Select</Link>
+					</button>
 				</>
 			);
 		} else {
